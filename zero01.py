5ea88a0643f4e797cb3aff9232bc8c15acc6b3fd
Demiao Chen <z5289988@ad.unsw.edu.au>
2021-11-06 16:36:18 +0800

    add empty model zero01, gitignore *.pth files

---
+++ zero01.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+"""
+student.py
+
+UNSW COMP9444 Neural Networks and Deep Learning
+
+You may modify this file however you wish, including creating additional
+variables, functions, classes, etc., so long as your code runs with the
+hw2main.py file unmodified, and you are only using the approved packages.
+
+You have been given some default values for the variables train_val_split,
+batch_size as well as the transform function.
+You are encouraged to modify these to improve the performance of your model.
+
+"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+
+"""
+   Answer to Question:
+
+Briefly describe how your program works, and explain any design and training
+decisions you made along the way.
+"""
+
+############################################################################
+######     Specify transform(s) to be applied to the input images     ######
+############################################################################
+def transform(mode):
+    """
+    Called when loading the data. Visit this URL for more information:
+    https://pytorch.org/vision/stable/transforms.html
+    You may specify different transforms for training and testing
+    """
+    if mode == 'train':
+        return transforms.ToTensor()
+    elif mode == 'test':
+        return transforms.ToTensor()
+
+
+############################################################################
+######   Define the Module to process the images and produce labels   ######
+############################################################################
+class Network(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        
+    def forward(self, input):
+        pass
+
+net = Network()
+    
+############################################################################
+######      Specify the optimizer and loss function                   ######
+############################################################################
+optimizer = None
+
+loss_func = None
+
+
+############################################################################
+######  Custom weight initialization and lr scheduling are optional   ######
+############################################################################
+
+# Normally, the default weight initialization and fixed learing rate
+# should work fine. But, we have made it possible for you to define
+# your own custom weight initialization and lr scheduler, if you wish.
+def weights_init(m):
+    return
+
+scheduler = None
+
+############################################################################
+#######              Metaparameters and training options              ######
+############################################################################
+dataset = "./data"
+train_val_split = 0.8
+batch_size = 200
+epochs = 10
